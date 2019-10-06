284a89dee88d1b825c63e03559d7442885dd7088
Matthew Ballance <matt.ballance@gmail.com>
2019-10-06 15:37:15 -0400

    Add cocotb tool definition

---
+++ mkfiles/common_tool_cocotb.mk
@@ -0,0 +1,28 @@
+#********************************************************************
+#* common_tool_cocotb.mk
+#********************************************************************
+ifneq (1,$(RULES))
+
+BUILD_COMPILE_TARGETS += build-cocotb-libs
+USER_DIR=$(BUILD_DIR)/cocotb
+export USER_DIR
+#COCOTB_SHARE_DIR = $(shell /usr/bin/env cocotb-config --share)
+
+VPI_LIBRARIES += $(BUILD_DIR)/cocotb/build/libs/x86_64/cocotb.vpi
+LD_LIBRARY_PATH:=$(BUILD_DIR)/cocotb/build/libs/x86_64:$(LD_LIBRARY_PATH)
+export LD_LIBRARY_PATH
+
+PYTHONPATH:=$(BUILD_DIR)/cocotb/build/libs/x86_64:$(PYTHONPATH)
+export PYTHONPATH
+
+#COCOTB_SIM=icarus
+#export COCOTB_SIM
+
+else
+
+build-cocotb-libs :
+	$(Q)COCOTB_SHARE_DIR=`cocotb-config --share` ; \
+		$(MAKE) -j1 -f $$COCOTB_SHARE_DIR/makefiles/Makefile.lib
+
+endif
+
